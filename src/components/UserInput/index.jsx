ac1c12055e44c4e7dc97412bf600ab8a89aba6ea
danielyefet <daniel.yefet@gmail.com>
2021-05-23 20:54:50 +0100

    Add send / recieve svg

--- src/components/UserInput/index.jsx
+++ src/components/UserInput/index.jsx
@@ -10,9 +10,11 @@ function UserInput({ onSubmit }) {
   });
 
   function handleChange({ target }) {
+    const id = target.getAttribute('id');
+
     setInputValues({
       ...inputValues,
-      [target.name]: target.type === 'checkbox' ? target.checked : target.value,
+      [id]: id === 'received' ? !inputValues.received : target.value,
     });
   }
 
@@ -23,14 +25,43 @@ function UserInput({ onSubmit }) {
       ...inputValues,
       timestamp: new Date().getTime(),
     });
+
+    setInputValues({
+      ...inputValues,
+      body: '',
+    });
   }
 
   return (
     <form className={styles.UserInput} onSubmit={handleSubmit}>
-      <input className={styles.input} name="name" type="text" value={inputValues.name} onChange={handleChange} />
-      <input className={styles.input} name="body" type="text" value={inputValues.body} onChange={handleChange} />
-      <input className={styles.input} name="received" type="checkbox" checked={inputValues.received} onChange={handleChange} />
-      <button type="submit">Send</button>
+      <input
+        className={styles.input}
+        id="name"
+        onChange={handleChange}
+        placeholder="Name..."
+        type="text"
+        value={inputValues.name}
+      />
+      <input
+        className={styles.input}
+        id="body"
+        onChange={handleChange}
+        placeholder="Message..."
+        type="text"
+        value={inputValues.body}
+      />
+      <button className={styles.button} type="submit">{inputValues.received ? 'Recieve' : 'Send'}</button>
+      <svg
+        className={styles.sendRecieveIcon}
+        fill="none"
+        id="received"
+        onClick={handleChange}
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4" />
+      </svg>
     </form>
   );
 }
