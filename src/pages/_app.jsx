77f4eaa94e9bcf3cb02a97033f496c9c2f40473f
Daniel Yefet <daniel.yefet@wintechnologies.net>
2021-05-24 23:14:21 +0100

    Use max-width for container

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,7 +4,7 @@ import '../styles.scss';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <div className="container mx-auto p-2">
+    <div className="container mx-auto p-2 max-w-screen-lg">
       <Component {...pageProps} />
     </div>
   );
