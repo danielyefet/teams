c66b0685f45be8b1aaeb196bfc9fc59fd0e20c92
danielyefet <daniel.yefet@gmail.com>
2021-06-11 23:19:37 +0100

    Add GA

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,23 @@
+/* eslint-disable react/no-danger */
 import '../styles.scss';
+
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <div className="container mx-auto p-2 max-w-screen-lg">
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-65PXEVSDKT" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){ dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'G-65PXEVSDKT');
+          `,
+        }}
+        />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
