87a31529f784f2f3e88770a1addefbe7fca1768a
danielyefet <daniel.yefet@gmail.com>
2021-06-11 23:25:08 +0100

    Add favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,6 +7,10 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <div className="container mx-auto p-2 max-w-screen-lg">
       <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-65PXEVSDKT" />
         <script dangerouslySetInnerHTML={{
           __html: `
