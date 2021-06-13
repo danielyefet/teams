087991456f52c07ad1b71c7cd0a407c4e8f22f32
danielyefet <daniel.yefet@gmail.com>
2021-06-13 23:35:57 +0100

    Add status

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./src/pages/**/*.{js,jsx}', './src/components/**/*.{js,jsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'teams-bg-dark': '#201F1F',
+        'teams-bg-light': '#F5F5F5',
+        'teams-status-available': '#6BB800',
+      },
+    },
     fontFamily: {
       teams: ['Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif'],
     },
