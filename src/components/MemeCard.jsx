58e51adcd9530c0d5da3b233b8100f0eab2e3bfb
danielyefet <daniel.yefet@gmail.com>
2021-08-12 22:30:18 +0100

    Use a tag for memes grid links

--- src/components/MemeCard.jsx
+++ src/components/MemeCard.jsx
@@ -6,17 +6,19 @@ function MemeCard({ image = '/', slug, title }) {
     <Link
       href={`/memes/${slug}`}
     >
-      <div className="transform-gpu hover:scale-105 hover:shadow-lg transition-all bg-white rounded shadow-md cursor-pointer overflow-hidden">
-        <div className="h-40 relative border-b border-gray-100">
-          <Image
-            alt={title}
-            layout="fill"
-            objectFit="cover"
-            src={image}
-          />
+      <a>
+        <div className="transform-gpu hover:scale-105 hover:shadow-lg transition-all bg-white rounded shadow-md cursor-pointer overflow-hidden">
+          <div className="h-40 relative border-b border-gray-100">
+            <Image
+              alt={title}
+              layout="fill"
+              objectFit="cover"
+              src={image}
+            />
+          </div>
+          <h3 className="text-gray-800 text-left text-sm font-normal px-4 py-3 overflow-hidden overflow-ellipsis whitespace-nowrap">{title}</h3>
         </div>
-        <h3 className="text-gray-800 text-left text-sm font-normal px-4 py-3 overflow-hidden overflow-ellipsis whitespace-nowrap">{title}</h3>
-      </div>
+      </a>
     </Link>
   );
 }
